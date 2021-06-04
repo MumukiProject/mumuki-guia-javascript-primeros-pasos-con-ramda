c019792e3746fe3618609590296fc993f4b4f196
federico.aloi@gmail.com <federico.aloi@gmail.com>
2021-06-04 20:18:29 +0000

    Mumuki Export on 2021-06-04 20:18:29 +0000

--- 00003_Orden superior/test..js
+++ 00003_Orden superior/test..js
@@ -1,20 +1,17 @@
 describe("losTriples", function() {
   it("de [1, 2, 3] es [3, 6, 9]", function() {
-    assert.equal(losTriples([1, 2, 3]), [3, 6, 9])
+    assert.deepEqual(losTriples([1, 2, 3]), [3, 6, 9])
   })
 })
 
 describe("lasIniciales", function() {
   it("de ['Andrea', 'Joaquín', 'Elmer'] son ['A', 'J', 'E']", function() {
-    assert.equal(lasIniciales(['Andrea', 'Joaquín', 'Elmer']), ['A', 'J', 'E'])
+    assert.deepEqual(lasIniciales(['Andrea', 'Joaquín', 'Elmer']), ['A', 'J', 'E'])
   })
 })
 
 describe("mayoresA10", function() {
-  it("12 sí lo es", function() {
-    assert(esMayorA10(12))
-  })
-  it("8 no lo es", function() {
-    assert(!esMayorA10(8))
+  it("quita los elementos menores", function() {
+    assert.deepEqual(mayoresA10([1, 12, 5, 22]), [12, 22])
   })
 })

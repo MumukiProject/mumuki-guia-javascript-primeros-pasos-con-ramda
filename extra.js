2e475e2fc1d0606afc0b3633df0717a080d30f94
federico.aloi@gmail.com <federico.aloi@gmail.com>
2021-06-05 12:56:42 +0000

    Mumuki Export on 2021-06-05 12:56:42 +0000

--- extra.js
+++ extra.js
@@ -1424,37 +1424,6 @@
     return a % b;
   });
 
-  /**
-   * A special placeholder value used to specify "gaps" within curried functions,
-   * allowing partial application of any combination of arguments, regardless of
-   * their positions.
-   *
-   * If `g` is a curried ternary function and `_` is `R.__`, the following are
-   * equivalent:
-   *
-   *   - `g(1, 2, 3)`
-   *   - `g(_, 2, 3)(1)`
-   *   - `g(_, _, 3)(1)(2)`
-   *   - `g(_, _, 3)(1, 2)`
-   *   - `g(_, 2, _)(1, 3)`
-   *   - `g(_, 2)(1)(3)`
-   *   - `g(_, 2)(1, 3)`
-   *   - `g(_, 2)(_, 3)(1)`
-   *
-   * @name __
-   * @constant
-   * @memberOf R
-   * @since v0.6.0
-   * @category Function
-   * @example
-   *
-   *      const greet = R.replace('{name}', R.__, 'Hello, {name}!');
-   *      greet('Alice'); //=> 'Hello, Alice!'
-   */
-  var __ = {
-    '@@functional/placeholder': true
-  };
-
   function _isFunction(x) {
     var type = Object.prototype.toString.call(x);
     return type === '[object Function]' || type === '[object AsyncFunction]' || type === '[object GeneratorFunction]' || type === '[object AsyncGeneratorFunction]';

dadf3deae851c09a4561b10d7834b4ca0e9c4f01
federico.aloi@gmail.com <federico.aloi@gmail.com>
2021-06-05 18:18:00 +0000

    Mumuki Export on 2021-06-05 18:18:00 +0000

--- 00004_Composición/test..js
+++ 00004_Composición/test..js
@@ -1,5 +1,17 @@
-describe("Descripción del grupo", function() {
-  it("Descripción del ejemplo", function() {
-    assert(true)
+describe("triplesMayoresA10", function() {
+  it("calcula los triples y luego filtra", function() {
+    assert.deepEqual(
+      triplesMayoresA10([3, 6, 9]), 
+      [18, 27]
+    )
+  })
+})
+
+describe("triplesDeLosMayores", function() {
+  it("filtra y luego calcula los triples", function() {
+    assert.deepEqual(
+      triplesDeLosMayores([8, 9, 10, 11]]), 
+      [33]
+    )
   })
 })

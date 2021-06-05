c4e7bf3ae7f9cb755aa0c853dae041ffc28c1a49
federico.aloi@gmail.com <federico.aloi@gmail.com>
2021-06-05 18:34:39 +0000

    Mumuki Export on 2021-06-05 18:34:39 +0000

--- 00005_Desafío final/extra.js
+++ 00005_Desafío final/extra.js
@@ -1,8 +1,8 @@
 const estudiantes = [
   { nombre: 'Fermín', edad: 23, 
     talleresAprobados: ['docker', 'funcional'] },
-  { nombre: 'Mara', edad: 19, talleresAprobados: [] },
-  { nombre: 'Lara', edad: 55, talleresAprobados: ['docker'] },
+  { nombre: 'Mara', edad: 55, talleresAprobados: [] },
+  { nombre: 'Lara', edad: 19, talleresAprobados: ['docker'] },
   { nombre: 'Arawi', edad: 18, 
     talleresAprobados: ['funcional'] },
   { nombre: 'Nahuel', edad: 31, 

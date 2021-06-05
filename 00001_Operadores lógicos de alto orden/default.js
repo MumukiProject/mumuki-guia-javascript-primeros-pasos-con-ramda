e9a626f5db363f4c94c447665c556bd49f929fb1
federico.aloi@gmail.com <federico.aloi@gmail.com>
2021-06-05 14:20:07 +0000

    Mumuki Export on 2021-06-05 14:20:07 +0000

--- 00001_Operadores lógicos de alto orden/default.js
+++ 00001_Operadores lógicos de alto orden/default.js
@@ -1,8 +1,8 @@
 // Es divisible por 4 y no es divisible por 100
-const cumpleCondicion1 = true 
+const cumpleCondicion1 = // COMPLETAR
 
 // Es divisible por 400
-const cumpleCondicion2 = true 
+const cumpleCondicion2 = // COMPLETAR
 
 // Combinar las funciones anteriores
-const esBisiesto = true 
+const esBisiesto = // COMPLETAR

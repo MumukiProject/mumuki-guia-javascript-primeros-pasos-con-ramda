e7a8431d4aa169f8f672fefe8ecbf9a57160ad9e
federico.aloi@gmail.com <federico.aloi@gmail.com>
2021-06-05 18:19:56 +0000

    Mumuki Export on 2021-06-05 18:19:56 +0000

--- 00004_Composición/extra.js
+++ 00004_Composición/extra.js
@@ -1,3 +1,7 @@
+const triple = (x) => x * 3
+const primeraLetra = (texto) => head(texto)
+const esMayorA10 = (x) => x > 10
+
 const losTriples = map(triple)
 const lasIniciales = map(primeraLetra)
 const mayoresA10 = filter(esMayorA10)
